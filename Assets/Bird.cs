20011e428de6a50d7bbe6e30167a3ab304ad9a67
Kushal Garg <kushalgarg2000@gmail.com>
2020-03-28 04:21:01 +0530

    Fixes collision box of bird and adds input script

---
+++ Assets/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Linked");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Flap");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right");
+        }
+    }
+}
